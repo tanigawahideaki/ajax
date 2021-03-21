0535e0bdfa58978229ae80d0f86383c81495c8d4
tanigawahideaki <80952139+tanigawahideaki@users.noreply.github.com>
2021-03-21 22:54:27 +0900

    Uplode file
    練習用の出力部分

---
+++ tpl/chat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>チャット</title>
+    </head>
+    <body>
+        <h1>チャット作れるかな？</h1>
+        <div id="chat"></div>
+        <input type="text" name="name" id="name" value="" placeholder="名前">
+        <input type="text" name="message" id="message" value="" placeholder="メッセージ">
+        <input type="button" id="click" value="送信">
+
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/chat.js"></script>
+    </body>
+</html>
